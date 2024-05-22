2e4bfab4245109d15cb2e7b4bffc70baa8ab4a29
Askirbst <tsbriks@gmail.com>
2024-05-22 18:47:11 -0400

    Still working on marbling simulation
    Marbling simulation program.

--- Class Project/Vector.py
+++ Class Project/Vector.py
@@ -3,6 +3,8 @@ import math
 
 class Vector:
     def __init__(self, x, y):
+        self.x = x
+        self.y = x
         self.vector = pygame.math.Vector2(x, y)
         # returns a vector for x and y
 
@@ -12,15 +14,41 @@ class Vector:
         
         self.vector *= scalar
 
-    def add(self, x, y):
+    def add(self, x=None, y=None, vec=None):
         # adds x and y to the vector allowing placement of drop where mouse clicked
-        other = pygame.math.Vector2(x, y)
+        if vec is not None:
+            self.x = vec.x
+            self.y = vec.y
+        else:
+            if x is not None:
+                self.x = x
+            if y is not None:
+                self.y = y
+        other = pygame.math.Vector2(self.x, self.y)
         self.vector += other
 
-    def sub(self, x, y):
+    def sub(self, x=None, y=None, vec=None):
         # subtracts another vector or x and y components from the current vector.
-        other = pygame.math.Vector2(x, y)
+        if vec is not None:
+            self.x = vec.x
+            self.y = vec.y
+        else:
+            if x is not None:
+                self.x = x
+            if y is not None:
+                self.y = y
+        other = pygame.math.Vector2(self.x, self.y)
         self.vector -= other
+    
+    def set(self, x=None, y=None, vec=None):
+        if vec is not None:
+            self.x = vec.x
+            self.y = vec.y
+        else:
+            if x is not None:
+                self.x = x
+            if y is not None:
+                self.y = y
 
     def getX(self):
         # Get the x coordinate of the vector
@@ -30,11 +58,9 @@ class Vector:
         # Get the y coordinate of the vector
         return self.vector.y
     
-    def mag(self, vertice1, vertice2):
-        # Get the magnitude of two coordinates
-        x1, y1 = vertice1
-        x2, y2 = vertice2
-        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    def mag(self):
+        # Get the magnitude of the coordinate
+        return math.sqrt(self.x**2 + self.y**2)
 
 
     def __str__(self):
--- Class Project/main.py
+++ Class Project/main.py
@@ -19,20 +19,10 @@ radius = 50
 angle_increment = 2 * math.pi / circleDetail
 
 def mapValue(value, start1, stop1, start2, stop2):
-    """
-    Maps a value from one range to another.
     
-    Parameters:
-    - value: The value to be mapped.
-    - start1, stop1: The range in which the value currently lies.
-    - start2, stop2: The range in which the value should be mapped.
-
-    """
+    # Maps a value from one range to another.
+    
     return ((value-start1)/(stop1-start1))*(stop2-start2)+start2
-
-def Cloning(li1): 
-    li_copy = li1
-    return li_copy 
 
 class Drop:
 
@@ -42,42 +32,47 @@ class Drop:
         self.s = s
 
         self.vertices = []
+        self.polygonUsage = [] # Effectively a copy of the vertices but in number pairs to be used in draw.polygon()
 
         for i in range(s):
             angle = mapValue(i, 0, s, 0, TWO_PI)
-            vector = Vector(math.cos(angle), math.sin(angle)) # Confusing math I don't quite understand yet but its works so...
+            vector = Vector(math.cos(angle), math.sin(angle)) # Confusing math I don't quite understand yet but it works so...
             vector.mult(self.r) # Multiplies the points in vector to the radius to scale the drop
             vector.add(x, y) # Add the value of the x, y coordinate where the mouse clicked to the values in vector
-            self.vertices.append((vector.getX(), vector.getY()))
+            self.polygonUsage.append((vector.getX(), vector.getY()))
+            self.vertices.append(vector)
 
     def drawDrop(self):
-        pygame.draw.polygon(screen, 'black', self.vertices)
+        pygame.draw.polygon(screen, 'black', self.polygonUsage)
 
     def marble(self, other):
         for v in self.vertices:
-            self.center = other
-            p = self.vertices
-            m = Vector.mag(self, self.vertices, p)
-            other = Vector.sub(p)
-
-
+            c = other.center
+            r = other.r
+            p = copy.copy(v)
+            p.sub(vec=v)
+            m = v.mag()
+            root = math.sqrt(1 + (r * r) / (m * m))
+            p.mult(root)
+            p.add(vec=c)
+            v.set(vec=p)
     
 def createDrop(x, y):
     drop = Drop(x, y, radius, circleDetail)
     for other in drops:
-        other.marble(drops)
+        drop.marble(other)
     drops.append(drop)
 
 
 while running:
     # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # pygame.QUIT event means the user clicked X to close the window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # Check if the left mouse button is clicked
-            if event.button == 1:  # Left mouse button
+            if event.button == 1:  
                 # Get the position of the mouse click
                 mouseX, mouseY = pygame.mouse.get_pos()
                 createDrop(mouseX, mouseY)
