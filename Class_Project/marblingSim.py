1a11a80a0f81f4ac31e74b9411a9be493ba53057
Askirbst <tsbriks@gmail.com>
2024-05-29 17:00:42 -0400

    Update to marblingSim.py
    *Final Update.
    -No longer a random color. User has control of which color to use with a button. Button displays color as the color of the button itself.
    -Click and drag functionality to create drops.
    -Limit on number of drops on screen.

--- Class_Project/marblingSim.py
+++ Class_Project/marblingSim.py
@@ -15,9 +15,12 @@ MAX_DROPS = 250
 drops = []
 circleDetail = 300
 radius = 50
+
 drag = False
 left_clicked = False
 right_clicked = False
+
+color = ["black", "green", "purple", "yellow", "red", "violet", "orange", "indigo"]
 
 angle_increment = 2 * math.pi / circleDetail
 
@@ -29,10 +32,13 @@ def mapValue(value, start1, stop1, start2, stop2):
 
 class Drop:
 
-    def __init__(self, x, y, r, s): # Initialize a Class object with parameters (x, y, r, s) x and y are coordinates, r is the radius, and s is smoothing of circle (number of sides of polygon)
+    def __init__(self, x, y, r, s, pick): # Initialize a Class object with parameters (x, y, r, s) x and y are coordinates, r is the radius, and s is smoothing of circle (number of sides of polygon)
         self.center = pygame.Vector2(x,y)
         self.r = r
         self.s = s
+
+        self.dropColor = color[pick]
+
 
         # Randomize a color for each drop
         self.colorR = random.randint(0, 255)
@@ -49,8 +55,8 @@ class Drop:
             self.vertices.append(self.vector)
 
     def drawDrop(self):
-
-        pygame.draw.polygon(screen, (self.colorR, self.colorG, self.colorB), self.vertices)
+        
+        pygame.draw.polygon(screen, self.dropColor, self.vertices)
 
     def marble(self, other):
         for v in self.vertices:
@@ -77,8 +83,8 @@ def tineLine(m, x, y, z, c):
     for drop in drops:
         drop.tine(m, x, y, z, c)
 
-def createDrop(x, y):
-    drop = Drop(x, y, radius, circleDetail)
+def createDrop(x, y, pick):
+    drop = Drop(x, y, radius, circleDetail, pick)
     for other in drops:
         other.marble(drop)
     if len(drops) >= MAX_DROPS:
@@ -101,6 +107,11 @@ def direction_unit_vector(coord1, coord2):
 start_posXY = []
 end_posXY = []
 
+buttonWidth = 100
+buttonHeight = 50
+buttonOffset = 10
+pick = 2
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close the window
@@ -108,7 +119,14 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             # Check if the left mouse button is clicked
             if event.button == 1: 
-                left_clicked = True  
+                mouseX, mouseY = pygame.mouse.get_pos()
+                if buttonWidth + buttonOffset >= mouseX >= buttonOffset and buttonHeight + buttonOffset >= mouseY >= buttonOffset:
+                    if pick >= 7:
+                        pick = 0
+                    else:
+                        pick += 1
+                else:
+                    left_clicked = True  
             if event.button == 3:
                 start_posXY = pygame.mouse.get_pos()
 
@@ -128,13 +146,17 @@ while running:
 
     if left_clicked:
         mouseX, mouseY = pygame.mouse.get_pos()
-        createDrop(mouseX, mouseY)  
+        createDrop(mouseX, mouseY, pick)  
     
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
+
     # draws all drops in the drops list
     for drop in drops:
         drop.drawDrop()
+
+    # draws a button to change color of drops
+    pygame.draw.rect(screen, color[pick], pygame.Rect(buttonOffset, buttonOffset, buttonWidth, buttonHeight))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
